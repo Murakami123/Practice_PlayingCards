e922454f21dbed0c40409d7e17dfbab08ded167f
Murakami <snowmanappli123@gmail.com>
2020-09-19 16:33:00 +0900

    Dotween 追加とか

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour
+{
+    [SerializeField] Image image;
+    [SerializeField] Sprite frontSpr, backSpr;
+
+    public void Init()
+    {
+
+    }
+
+    //------------------------------------------------------------------
+    // クラス内の大項目名
+    //------------------------------------------------------------------
+
+
+}
---
+++ Assets/Scripts/Cardbase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using Cysharp.Threading.Tasks;
+
+public abstract class Cardbase : MonoBehaviour
+{
+    [SerializeField] Image image;
+    [SerializeField] Sprite frontSpr, backSpr;
+    public void Init()
+    {
+
+    }
+
+    public async UniTask ReturnCard(bool isShowFront)
+    {
+        transform.Dorotate();
+        await UniTask.Delay(1);
+        await transform.DOMove(transform.position + Vector3.up, 1.0f);
+    }
+
+
+}
