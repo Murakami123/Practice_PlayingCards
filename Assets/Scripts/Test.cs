c74bb9a1577a3f10d456399304a76486bd0d8fb3
Murakami <snowmanappli123@gmail.com>
2020-09-19 14:42:13 +0900

    UniTask導入

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cysharp.Threading.Tasks;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Hoge().Forget();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    private async UniTask Hoge()
+    {
+        Debug.Log("あああ");
+        await UniTask.Delay(5000);
+        Debug.Log("3秒待って実行");
+    }
+}
