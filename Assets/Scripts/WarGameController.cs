e92f3e0c78714cb7ce602b5a165da8e51f4caac1
Murakami <snowmanappli123@gmail.com>
2020-10-01 09:24:52 +0900

    UniTask.WhenAll をきれいに書く

--- Assets/Scripts/WarGameController.cs
+++ Assets/Scripts/WarGameController.cs
@@ -3,12 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cysharp.Threading.Tasks;
+using Cysharp.Threading.Tasks.Linq;
 
 public class WarGameController : MonoBehaviour
 {
     [SerializeField] private DeckController deckController;
     [SerializeField] private PlayerController[] playerController = new PlayerController[2]; // 2人対戦
-    [SerializeField] private RectTransform HikiwakeCardPatent; 
+    [SerializeField] private RectTransform HikiwakeCardPatent;
     private PlayerController player1 => playerController[0];
     private PlayerController player2 => playerController[1];
     private int useJokerCount = 2; // 1にしたいけど奇数だと最後1枚余る
@@ -25,30 +26,35 @@ public class WarGameController : MonoBehaviour
 
         // 山札なくなるまで対戦繰り返し
         Debug.Log("deckController.GetDeckCardCount:" + deckController.GetDeckCardCount);
-        while (deckController.GetDeckCardCount >0)
+        while (deckController.GetDeckCardCount > 0)
         {
             Debug.Log("残り山札枚数；" + deckController.GetDeckCardCount);
+
             // プレイヤーにカードを配る
-            var task1 = DistributeCard( player1 );
-            var task2 = DistributeCard( player2 );
-            await UniTask.WhenAll(task1, task2);
+            await (
+                DistributeCard(player1),
+                DistributeCard(player2)
+            );
 
             // 両方のプレイヤーが選択するの待ち
             player1.ReleaseChoiceCard();
             player2.ReleaseChoiceCard();
-            var task3 = player1.PlayerChoiceCard(isRemovePlayerCard:true);
-            var task4 = player2.PlayerChoiceCard(isRemovePlayerCard:true);
-            var choiceCards = await UniTask.WhenAll(task3, task4);
+
+            var choiceCards = await (
+                player1.PlayerChoiceCard(isRemovePlayerCard: true),
+                player2.PlayerChoiceCard(isRemovePlayerCard: true)
+            );
 
             // 両方のカードを表向きにする
             var player1Card = choiceCards.Item1;
             var player2Card = choiceCards.Item2;
             player1Card.SetChoice(false); // 選択中表示終了
-            player2Card.SetChoice(false); 
-            var task9  = player1Card.ReturnCard(isShowFront:true);
-            var task10 = player2Card.ReturnCard(isShowFront:true);
-            await UniTask.WhenAll(task9, task10);
-            
+            player2Card.SetChoice(false);
+            await (
+                player1Card.ReturnCard(isShowFront: true),
+                player2Card.ReturnCard(isShowFront: true)
+            );
+
             // 1回の対戦;
             if (player1Card.cardNo == player2Card.cardNo)
             {
@@ -56,19 +62,21 @@ public class WarGameController : MonoBehaviour
 
                 // 引き分け
                 var hikwakeCardPos = HikiwakeCardPatent.localPosition;
-                var task7 = player1Card.MoveCard(HikiwakeCardPatent, hikwakeCardPos, isLittleShit:true);
-                var task8 = player2Card.MoveCard(HikiwakeCardPatent, hikwakeCardPos, isLittleShit:true);
-                await UniTask.WhenAll(task7, task8);
+                await (
+                    player1Card.MoveCard(HikiwakeCardPatent, hikwakeCardPos, isLittleShit: true),
+                    player2Card.MoveCard(HikiwakeCardPatent, hikwakeCardPos, isLittleShit: true)
+                );
             }
             else
             {
                 // どっちか勝ち_カード移動
                 var winner = (player1Card.cardNo > player2Card.cardNo) ? player1 : player2;
                 Debug.Log("勝者:" + winner);
-                var task5 = winner.SetWinCard(player1Card);
-                var task6 = winner.SetWinCard(player2Card);
-                await UniTask.WhenAll(task5, task6);
-            }            
+                await (
+                    winner.SetWinCard(player1Card),
+                    winner.SetWinCard(player2Card)
+                );
+            }
         }
 
         // 勝敗引き分けチェック
@@ -82,7 +90,7 @@ public class WarGameController : MonoBehaviour
             var isPlayer1win = (player1.winGetCardList.Count > player2.winGetCardList.Count);
             if (isPlayer1win)
                 GameEnd_Player1Win();
-            else 
+            else
                 GameEnd_Player2Win();
         }
     }
@@ -97,11 +105,23 @@ public class WarGameController : MonoBehaviour
     /////////////////////////////////////////////////////
     // 勝敗表示
     /////////////////////////////////////////////////////    
-    [SerializeField] private GameObject player1WinObj, player2WinObj, gameHikiwakeObj; 
-    private void GameEnd_Player1Win(){ player1WinObj.SetActive(true);}
-    private void GameEnd_Player2Win(){ player2WinObj.SetActive(true);}
-    private void GameEnd_Hikiwake(){ gameHikiwakeObj.SetActive(true);}
-   
+    [SerializeField] private GameObject player1WinObj, player2WinObj, gameHikiwakeObj;
+
+    private void GameEnd_Player1Win()
+    {
+        player1WinObj.SetActive(true);
+    }
+
+    private void GameEnd_Player2Win()
+    {
+        player2WinObj.SetActive(true);
+    }
+
+    private void GameEnd_Hikiwake()
+    {
+        gameHikiwakeObj.SetActive(true);
+    }
+
     // ◆トランプゲーム共通であったらよさそうな概念
     // ○GameControllerBase
     //      ・（「実際のトランプの場合、ユーザが行うが自動で行った方がよいもの」等を実行する）
@@ -110,7 +130,7 @@ public class WarGameController : MonoBehaviour
     //      ・リトライ(GameRetryメソッド)
     // ○カードbase
     //      ・片面が数字と絵柄、片面がトランプの裏、ひっくり返すことができる
-    
+
     // ◆「戦争:GameControllerBase」に出てくる概念の洗い出し
     // ○WarGameController
     //      ・ゲームで使うジョーカーの枚数指定
@@ -143,5 +163,4 @@ public class WarGameController : MonoBehaviour
 
     // ◆トランプゲームはウォーターフォール型が多いから、シーケンス図が向いてる気がする。
     // 
-
 }
